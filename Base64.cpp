e4ad00c2ee1697e5d8afed5a83882444802cc59f
Jonas4420 <jonas.lejeune4420@gmail.com>
2017-11-16 15:40:27 +0900

    Add project architecture

---
+++ Base64.cpp
@@ -0,0 +1,18 @@
+#include "crypto/Base64.hpp"
+
+namespace Crypto
+{
+
+int
+Base64::encode()
+{
+	return 0;
+}
+
+int
+Base64::decode()
+{
+	return 1;
+}
+
+}
---
+++ include/crypto/Base64.hpp
@@ -0,0 +1,9 @@
+namespace Crypto
+{
+	class Base64
+	{
+		public:
+			int encode();
+			int decode();
+	};
+}
